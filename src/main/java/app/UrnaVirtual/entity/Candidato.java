5c5a6ccca578a75f6e71f341cd1bae9332db474a
joojkafer <joaoantoniokaferribeiro@gmail.com>
2024-10-24 23:09:39 -0300

    troca da annotation de cpf candidato para pattern

--- src/main/java/app/UrnaVirtual/entity/Candidato.java
+++ src/main/java/app/UrnaVirtual/entity/Candidato.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,8 +31,8 @@ public class Candidato {
     private String nomeCompleto;
 
     @Column(nullable = false, unique = true)
-    @CPF
     @NotNull
+    @Pattern(regexp = "^[0-9]{11}$", message = "CPF deve conter exatamente 11 dígitos numéricos")
     private String cpf;
 
     @Column(nullable = false, unique = true)
